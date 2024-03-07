503dbfdb32204577fed5e8ba11adfa1d62698b44
Ezequiel <qferro@gmail.com>
2024-03-07 06:29:23 -0300

    Datepicker casi full implementado

--- src/app/components/selec-fecha/selec-fecha.component.ts
+++ src/app/components/selec-fecha/selec-fecha.component.ts
@@ -1,5 +1,5 @@
-import { Component, Injectable } from '@angular/core';
-import { NgbAlertModule, NgbDatepickerModule, NgbDateStruct, NgbDatepickerI18n, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Injectable, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { NgbAlertModule, NgbDatepickerModule, NgbDateStruct, NgbDatepickerI18n, NgbCalendar, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
@@ -9,8 +9,6 @@ const I18N_VALUES = {
     months: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
   }
 };
-
-
 
 @Injectable()
 export class CustomDatepickerI18n extends NgbDatepickerI18n {
@@ -37,16 +35,59 @@ export class CustomDatepickerI18n extends NgbDatepickerI18n {
   styleUrls: ['./selec-fecha.component.css'],
   providers: [ { provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n }],
 })
-export class NgbdDatepickerI18n {
+export class NgbdDatepickerI18n implements OnChanges{
   model!: NgbDateStruct;  
   desde: NgbDateStruct;
+  fechasDisponibles!: NgbDateStruct[];
+  isDisabled:  any | boolean = false;
+  @Input() pcia_id?: string;
+  
+
   constructor(private calendario: NgbCalendar){
     this.desde = calendario.getToday();
+    console.log('Valor inicial de pcia_id',this.pcia_id);
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.pcia_id == '0'){
+      this.isDisabled = false;
+    }else if (changes['pcia_id'] ){
+    console.log(this.pcia_id);
+    if (this.pcia_id == '656e59c935a4cd190c93b4b7'){
+      this.fechasDisponibles = [
+        { year: 2024, month: 3, day:  11},
+        { year: 2024, month: 3, day:  14},
+        { year: 2024, month: 3, day:  18},
+        { year: 2024, month: 3, day: 19 },
+      ];
+    }else{
+      this.fechasDisponibles = [
+        { year: 2024, month: 3, day: 20},
+        { year: 2024, month: 3, day:  21},
+        { year: 2024, month: 3, day:  22},
+        { year: 2024, month: 3, day:25},
+      ];
+    }
+
+    this.isDisabled = (date: NgbDate): boolean => {
+      return !this.fechasDisponibles.some((fechaDisponible) => {
+        return this.isSameDate(date, fechaDisponible);
+      });
+    };
+    }
+
+  }
+  
   logModelValue() {
     // esta es la funcion que emite el valor de fecha2
     const dateString: string = new Date(this.model.year, this.model.month - 1, this.model.day).toLocaleDateString();
-    console.log(dateString);
+  }
+
+  isSameDate(date1: NgbDateStruct, date2: NgbDateStruct): boolean {
+    return (
+      date1.year === date2.year &&
+      date1.month === date2.month &&
+      date1.day === date2.day
+    );
   }
 
 }
