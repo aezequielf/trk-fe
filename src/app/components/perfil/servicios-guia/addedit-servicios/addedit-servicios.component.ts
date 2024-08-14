b320c9b636b1482bce56e80bdce0a7ae77e77d93
Ezequiel <qferro@gmail.com>
2024-08-14 15:19:55 -0300

    Agregado arreglo de fechas

--- src/app/components/perfil/servicios-guia/addedit-servicios/addedit-servicios.component.ts
+++ src/app/components/perfil/servicios-guia/addedit-servicios/addedit-servicios.component.ts
@@ -1,7 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { Travesia } from 'src/app/models/interfaces-travesia';
+import { Travesia} from 'src/app/models/interfaces-travesia';
 import { TravesiaServicioService } from 'src/app/services/travesia-servicio.service';
 
 @Component({
@@ -9,6 +9,9 @@ import { TravesiaServicioService } from 'src/app/services/travesia-servicio.serv
   templateUrl: './addedit-servicios.component.html',
   styleUrls: ['./addedit-servicios.component.css']
 })
+
+
+
 export class AddeditServiciosComponent implements OnInit{
 
   constructor(private tstmsj: ToastrService, private srvtravesia : TravesiaServicioService, private tuboFecha: DatePipe){}
@@ -16,11 +19,13 @@ export class AddeditServiciosComponent implements OnInit{
   ngOnInit(): void {
     if (this.in_travesia != undefined){
       this.travesia = this.in_travesia;
-      this.travesia.fecha = this.tuboFecha.transform(this.travesia.fecha ,'yyyy-MM-dd')!;
+      this.nuevaFecha = this.tuboFecha.transform(this.travesia.fecha ,'yyyy-MM-dd')!;
     }
     
   }
   
+  nuevaFecha = '';
+  fechas : string[] = [];
   
 
   @Output() cancelaCarga= new EventEmitter();
@@ -73,12 +78,34 @@ export class AddeditServiciosComponent implements OnInit{
     precio: 0
   };
   
+  // redefino interface travesía en travesia2 para poder enviar un array en fecha
+
   pagina = 1;
 
   cancelar(){
     this.cancelaCarga.emit();
   }
 
+  agregaFechas(){
+    let fechaHoy = new Date();
+    if(this.nuevaFecha == ''){
+      this.tstmsj.warning('No puedo agregar una fecha vacía ...');
+      return;    
+    }
+    if (this.fechas.includes(this.nuevaFecha)){
+      this.tstmsj.warning('Fecha ya en lista !!!');
+      return;
+    }
+    if (Date.parse(this.nuevaFecha) <= fechaHoy.getTime() ){
+      this.tstmsj.warning('No se pueden agregar fechas pasadas o agregar el día de hoy !!!');
+      return;
+    }
+    this.fechas.push(this.nuevaFecha);
+  }
+
+  quitaFechas(indice: number){
+    this.fechas.splice(indice,1);
+  }
 
   guardar(){
     // cargo a mano algunos valores por ahora antes de resolver como los hago coherentes.
@@ -101,12 +128,14 @@ export class AddeditServiciosComponent implements OnInit{
       return
     }
 
-
-    this.srvtravesia.nueva_travesia_guia(this.travesia).subscribe({
-      next: rta => this.tstmsj.success(`travesía ${rta.lugar} guardada para ${rta.empresa} `),
-      error: err =>  console.log(err),
-      complete: () => {this.ActualizaTravesias.emit(); this.cancelaCarga.emit()}
-    });
-    
+    for (let ind = 0 ; ind < this.fechas.length; ind++){
+      // aca ejecuto los post de acuerdo a la cantidad de fechas que existan
+      this.travesia.fecha = this.fechas[ind];
+      this.srvtravesia.nueva_travesia_guia(this.travesia).subscribe({
+        next: rta => this.tstmsj.success(`travesía ${rta.lugar}, fecha ${this.tuboFecha.transform(rta.fecha ,'dd/MM/yyyy')} guardada para ${rta.empresa} `),
+        error: err =>  console.log(err),
+        complete: () => {this.ActualizaTravesias.emit(); this.cancelaCarga.emit()}
+      });
+    }
   }
 }
